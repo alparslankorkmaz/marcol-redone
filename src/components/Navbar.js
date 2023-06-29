9146fdc550dc69ccf77bd9c09f3cf156dde0266c
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-06-29 16:27:15 +0100

    navbar outline

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <>
+      <header className="py-14 bg-gradient-to-b from-black to-transparent mx-auto px-3 lg:px-36 flex justify-between items-center">
+        <div>
+          <Link href="/">
+            <img
+              src="/images/marcol_logo.png"
+              alt="Marcol Logo"
+              className="w-48 h-auto"
+            />
+          </Link>
+        </div>
+        <div className="flex gap-8 tracking-wider font-extrabold text-white">
+          <Link href="/home" className="uppercase">
+            home
+          </Link>
+          <Link href="/about-us" className="uppercase">
+            about us
+          </Link>
+          <Link href="/services" className="uppercase">
+            services
+          </Link>
+          <Link href="/portfolio" className="uppercase">
+            portfolio
+          </Link>
+          <Link href="/technical" className="uppercase">
+            technical
+          </Link>
+          <Link href="/contact" className="uppercase">
+            contact
+          </Link>
+        </div>
+      </header>
+    </>
+  );
+}
