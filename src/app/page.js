ffb5217d15e87c1edb86e0192b9cf87f71739d44
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-06-29 16:40:20 +0100

    add unresponsive navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 import Link from "next/link";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
 }
