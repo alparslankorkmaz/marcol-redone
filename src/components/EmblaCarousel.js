2853429272dfc40b13f9801ad317a8380934bbfd
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-06-30 17:08:30 +0100

    finish hero carousel

--- src/components/EmblaCarousel.js
+++ src/components/EmblaCarousel.js
@@ -1,13 +1,14 @@
 import React from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
+import Link from "next/link";
 
 export default function EmblaCarousel() {
   const autoplayOptions = {
-    delay: 5000,
+    delay: 4500,
   };
   const [emblaRef] = useEmblaCarousel({ loop: true }, [
-    // Autoplay(autoplayOptions),
+    Autoplay(autoplayOptions),
   ]); // Add plugin
 
   return (
@@ -26,30 +27,81 @@ export default function EmblaCarousel() {
             </span>{" "}
             marcol fabrications
           </h1>
-          <button className="px-5 py-2 lg:px-9 lg:py-4 text-white bg-marcol-blue font-archivo text-xl font-light absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-28 uppercase text-center">
+          <Link
+            href="/contact"
+            className="px-5 py-2 lg:px-9 lg:py-4 text-white bg-marcol-blue font-archivo text-xl font-light absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-28 uppercase text-center"
+          >
             get in touch
-          </button>
+          </Link>
         </div>
-        <div className="embla__slide">
+        <div className="embla__slide relative">
           <img
             src="/images/carousel/carousel2.jpg"
             alt="bespoke fabrication"
             className="min-h-screen object-cover"
           />
+          <h2 className="font-black lg:whitespace-nowrap absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 uppercase text-center text-white text-5xl lg:text-7xl tracking-wide">
+            <span className="font-archivo block text-xl lg:text-4xl my-2 font-light">
+              {" "}
+              on budget{" "}
+            </span>{" "}
+            and on time
+            <span className="font-archivo block text-lg lg:text-2xl my-2 font-light">
+              whatever you need
+            </span>
+          </h2>
+          <Link
+            href="/contact"
+            className="px-5 py-2 lg:px-9 lg:py-4 text-white bg-marcol-blue font-archivo text-xl font-light absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-28 uppercase text-center"
+          >
+            get in touch
+          </Link>
         </div>
-        <div className="embla__slide">
+        <div className="embla__slide relative">
           <img
             src="/images/carousel/carousel3.jpg"
             alt="tanks and bunds"
             className="min-h-screen object-cover"
           />
+          <h3 className="font-black lg:whitespace-nowrap absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 uppercase text-center text-white text-5xl lg:text-7xl tracking-wide">
+            <span className="font-archivo block text-xl lg:text-4xl my-2 font-light">
+              {" "}
+              a winning{" "}
+            </span>{" "}
+            combination
+            <span className="font-archivo block text-lg lg:text-2xl my-2 font-light">
+              combining excellence with experience
+            </span>
+          </h3>
+          <Link
+            href="/contact"
+            className="px-5 py-2 lg:px-9 lg:py-4 text-white bg-marcol-blue font-archivo text-xl font-light absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-28 uppercase text-center"
+          >
+            get in touch
+          </Link>
         </div>
-        <div className="embla__slide">
+        <div className="embla__slide relative">
           <img
             src="/images/carousel/carousel4.jpg"
             alt="pipework"
             className="min-h-screen object-cover"
           />
+          <h4 className="font-black lg:whitespace-nowrap absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 uppercase text-center text-white text-5xl lg:text-7xl tracking-wide">
+            <span className="font-archivo block text-xl lg:text-4xl my-2 font-light">
+              {" "}
+              no matter{" "}
+            </span>{" "}
+            how big or small
+            <span className="font-archivo block text-lg lg:text-2xl my-2 font-light">
+              delivered to your door
+            </span>
+          </h4>
+          <Link
+            href="/contact"
+            className="px-5 py-2 lg:px-9 lg:py-4 text-white bg-marcol-blue font-archivo text-xl font-light absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-28 uppercase text-center"
+          >
+            get in touch
+          </Link>
         </div>
       </div>
     </div>
