88fa8c9298153230e7887aa3447885038199c662
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-07-06 17:17:54 +0100

    add routing on whatwedo component

--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -5,7 +5,7 @@ export default function WhatWeDo() {
     <>
       <div className="portfolio grid grid-cols-4 gap-10 p-10 lg:p-20">
         <div className="scrubbing-systems flex justify-center col-span-4 lg:col-span-1">
-          <Link href="/" className="relative">
+          <Link href={`/portfolio#scrubbing-systems`} className="relative">
             <img
               src="/images/what_we_do/scrubbing_systems.jpg"
               alt="scrubbing systems"
@@ -16,7 +16,7 @@ export default function WhatWeDo() {
           </Link>
         </div>
         <div className="enclosures flex justify-center col-span-4 lg:col-span-1">
-          <Link href="/" className="relative">
+          <Link href={`/portfolio#enclosures`} className="relative">
             <img src="/images/what_we_do/enclosures.jpg" alt="enclosures" />
             <div className="absolute border-b-4 p-3 font-black text-xl lg:text-base tracking-wider whitespace-nowrap text-white bottom-4 left-1/2 -translate-x-1/2 uppercase">
               enclosures
@@ -24,7 +24,7 @@ export default function WhatWeDo() {
           </Link>
         </div>
         <div className="tanks-bunds flex justify-center col-span-4 lg:col-span-1">
-          <Link href="/" className="relative">
+          <Link href={`/portfolio#tanks-and-bunds`} className="relative">
             <img
               src="/images/what_we_do/tanks_bunds.jpg"
               alt="tanks and bunds"
@@ -35,7 +35,10 @@ export default function WhatWeDo() {
           </Link>
         </div>
         <div className="extraction-systems flex justify-center col-span-4 lg:col-span-1">
-          <Link href="/" className="relative">
+          <Link
+            href={`/portfolio#ducting-and-extraction-systems`}
+            className="relative"
+          >
             <img
               src="/images/what_we_do/ducting_extraction.jpg"
               alt="extraction systems"
@@ -46,7 +49,7 @@ export default function WhatWeDo() {
           </Link>
         </div>
         <div className="machine-guards flex justify-center col-span-4 lg:col-span-1">
-          <Link href="/" className="relative">
+          <Link href={`/portfolio#machine-guards`} className="relative">
             <img
               src="/images/what_we_do/machine_guards.jpg"
               alt="machine guards"
@@ -57,7 +60,7 @@ export default function WhatWeDo() {
           </Link>
         </div>
         <div className="pipework flex justify-center col-span-4 lg:col-span-1">
-          <Link href="/" className="relative">
+          <Link href={`/portfolio#pipework`} className="relative">
             <img src="/images/what_we_do/pipework.jpg" alt="pipework" />
             <div className="absolute border-b-4 p-3 font-black text-xl lg:text-base tracking-wider whitespace-nowrap text-white bottom-4 left-1/2 -translate-x-1/2 uppercase">
               pipework
@@ -65,7 +68,7 @@ export default function WhatWeDo() {
           </Link>
         </div>
         <div className="bespoke-fabrication flex justify-center col-span-4 lg:col-span-1">
-          <Link href="/" className="relative">
+          <Link href={`/portfolio#bespoke-fabrication`} className="relative">
             <img
               src="/images/what_we_do/bespoke_fabrication.jpg"
               alt="bespoke fabrication"
@@ -76,7 +79,7 @@ export default function WhatWeDo() {
           </Link>
         </div>
         <div className="installation flex justify-center col-span-4 lg:col-span-1">
-          <Link href="/" className="relative">
+          <Link href={`/portfolio#installation`} className="relative">
             <img src="/images/what_we_do/installation.jpg" alt="installation" />
             <div className="absolute border-b-4 p-3 font-black text-xl lg:text-base tracking-wider whitespace-nowrap text-white bottom-4 left-1/2 -translate-x-1/2 uppercase">
               installation
